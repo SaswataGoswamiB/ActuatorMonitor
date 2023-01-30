025a613629b2e0bb651d3d61f54f0de9ccb1b05f
SaswataGoswami <85555421+SaswataGoswami@users.noreply.github.com>
2023-01-30 18:16:00 +0530

    adding home COntrooler

---
+++ actuator-example/src/main/java/com/actuator/example/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.actuator.example.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	public Map<String,String> getdata()
+	{
+		
+	}
+
+}

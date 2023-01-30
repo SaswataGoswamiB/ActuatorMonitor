053fa2f3f86b220fa80d461b6b70bbd7c92ac585
SaswataGoswami <85555421+SaswataGoswami@users.noreply.github.com>
2023-01-30 18:39:22 +0530

    Updating POm with Actuator dependency.

---
+++ actuator-example/src/main/java/com/actuator/example/Entity/Student.java
@@ -0,0 +1,13 @@
+package com.actuator.example.Entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	
+
+	public Student () 
+	{
+		System.out.println("Creating the object of Studnet");
+	}
+}
--- actuator-example/src/main/java/com/actuator/example/controller/HomeController.java
+++ actuator-example/src/main/java/com/actuator/example/controller/HomeController.java
@@ -2,14 +2,22 @@ package com.actuator.example.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.actuator.example.Entity.Student;
 
 @RestController
 public class HomeController {
 	
+	@Autowired
+	private Student student;
+	
+	@GetMapping("/getdata")
 	public Map<String,String> getdata()
 	{
-		
+		return Map.of("Name","Saswata");
 	}
 
 }

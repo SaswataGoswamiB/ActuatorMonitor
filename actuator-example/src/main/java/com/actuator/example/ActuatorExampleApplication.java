dce3467ac9ce75e624ac6ba13f4be7683dad4351
SaswataGoswami <85555421+SaswataGoswami@users.noreply.github.com>
2023-01-30 17:42:34 +0530

    initial commit

---
+++ actuator-example/src/main/java/com/actuator/example/ActuatorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.actuator.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorExampleApplication.class, args);
+	}
+
+}
